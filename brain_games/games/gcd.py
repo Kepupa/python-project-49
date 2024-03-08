5f2536cfdb117f12d191320f4edffa9f15b00c21
Kepupa <2Sa1kle2@mail.ru>
2024-03-08 16:42:15 +0300

    update gcd

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,13 +1,20 @@
 from random import randint
-from math import gcd
+
 
 
 rules = 'Find the greatest common divisor of given numbers.'
+#Алгорит Евклида, выглядит сложно, но прикольно! 
+def gcd(num1, num2):
+    max_val = max(num1, num2)
+    min_val = min(num1, num2)
 
-def play_game():
-    num_1 = randint(1, 100)
-    num_2 = randint(1, 100)
-    correct_answer = str(gcd(num_1, num_2))
-    num = f'{num_1} {num_2}'
+    while min_val:
+        max_val, min_val = min_val, max_val % min_val
+    return max_val
 
+def gplay_game():
+    num1 = randint(0, 100)
+    num2 = randint(0, 100)
+    num = f"{num1} {num2}"
+    correct_answer = gcd(num1, num2)
     return correct_answer, num
